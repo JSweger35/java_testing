6ce5989f61eb3652a5b37eb918a2b2e509afe5af
jsweger <jsweg13@gmail.com>
2023-02-14 13:07:09 -0500

    Add lesson_7 class

---
+++ Lesson_7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+/**
+ * Lesson 7 activites.
+ * 
+ * @author Jake Sweger
+ * @version 1.0
+ */
+public class Lesson_7 {
+    /**
+     * main() reads Fahrenheit temperatures and 
+     * displays their Celsius and Kelvin equivalents.
+     */
+    public static void main(String[] args) {
+        
+    }
+}
