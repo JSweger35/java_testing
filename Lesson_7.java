e1811fabeba8a97924ff184e10c154022d557765
jsweger <jsweg13@gmail.com>
2023-02-14 13:23:09 -0500

    Add loops to print x and y

--- Lesson_7.java
+++ Lesson_7.java
@@ -11,6 +11,15 @@ public class Lesson_7 {
      * displays their Celsius and Kelvin equivalents.
      */
     public static void main(String[] args) {
-        
+        int x = 0;
+        do {
+            System.out.println(x);
+            x++;
+        } while(x < 10);
+        int y = 0;
+        while (y < 10){
+            System.out.println(y);
+            y++; 
+        }
     }
 }
